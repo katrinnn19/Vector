f57fab0d7c4548590b8bbd93a8a1dde3f95e89b2
katrinnn19 <lgfox941@gmail.com>
2024-04-25 01:14:38 +0200

    Tri_o_ne_3
    3

---
+++ Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 2f;
+
+    void Start() {
+    }
+
+    private void LateUpdate() {
+        direction = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if(direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+    }
+}
